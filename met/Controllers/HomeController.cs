72074a53fd5161b89c25fa3fb1d01719be2a1fcc
TheBoyCode <olexandermelnyk@gmail.com>
2018-08-27 10:45:36 +0300

    web api first commit

---
+++ met/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using ASPNetCoreForms.models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace met.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(LoginModel model)
+        {
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                message = "ID " + model.ID + " Login " + model.Login + " Password " + model.Password + " created successfully";
+            }
+            else
+            {
+                message = "Failed to create the product. Please try again";
+            }
+            return Content(message);
+        }
+    }
+}
