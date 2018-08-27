c3632a4eeb99892c8a54bc80eb832efd6b118b76
TheBoyCode <olexandermelnyk@gmail.com>
2018-08-27 11:09:41 +0300

    add models

---
+++ met/Models/QuestionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace met.Models
+{
+    public class QuestionModel
+    {
+        public string ID { get; set; }
+        public string Question { get; set; }
+    }
+}
---
+++ met/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace met.Models
+{
+    public class UserModel
+    {
+        public string ID { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public List<QuestionModel> Questions { get; set; }
+    }
+}
